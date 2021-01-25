7d8b9a3773a62adfba88a43dee99705f58b40a33
Joshaline <joshuachinwendu@gmail.com>
2021-01-25 11:16:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+//-----IMPORTS-----//
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+const mongoose = require('mongoose');
+
+//-----Connecting mongoose-----//
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser:true})
+
+//------INITIALIZATION------//
+const app = new express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.listen(3000, () => {
+  console.log("App listening on port 3000")
+})
+
+//------ROUTES--------//
+app.get('/', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+  res.render('index');
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+app.get('/post', (req, res) => {
+  res.render('post')
+})
+
+
+
+
+
+
+//
